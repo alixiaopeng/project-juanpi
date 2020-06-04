6ba9444c3b7da6cb38c6660fd41bcce86d6e01d7
zhujiapeng <297071593@qq.com>
2020-06-04 21:08:41 +0800

    完成了列表页的分页功能

--- php/listdata.php
+++ php/listdata.php
@@ -1,11 +1,25 @@
 <?php
 include "conn.php";
 
-$result = $conn->query("select * from allgoodlist");
+$pagesize = 10; //单个页面展示的数据条数
+$result = $conn->query("select * from allgoodlist");//记录集
+$num = $result->num_rows;//总条数
+$pagenum = ceil($num / $pagesize); //页数
+
+//获取前端的传来的页面，根据页码查询对应的数据，返回给前端。
+if (isset($_GET['page'])) {
+    $pagevalue = $_GET['page'];
+} else {
+    $pagevalue = 1;
+}
+$page = ($pagevalue - 1) * $pagesize;
+
+$sql1 = "select * from allgoodlist limit $page,$pagesize";
+$res = $conn->query($sql1);
 
 $arrdata = array();
-for ($i = 0; $i < $result->num_rows; $i++) {
-    $arrdata[$i] = $result->fetch_assoc();
+for ($i = 0; $i < $res->num_rows; $i++) {
+    $arrdata[$i] = $res->fetch_assoc();
 }
 
 echo json_encode($arrdata);
--- src/js/list.js
+++ src/js/list.js
@@ -1,90 +1,134 @@
-class List {
-	constructor() {
-        this.toolbar = $(".toolbar");
-        this.header = $("#header");
-		this.footer = $("#footer");
-        this.brandList = $(".brand-list"); //商品列表部分
-        this.signOutBtn = $(".sign-out"); //退出按钮
-	}
+(function () {
+	class List {
+		constructor() {
+			this.toolbar = $(".toolbar");
+			this.header = $("#header");
+			this.footer = $("#footer");
+			this.brandList = $(".brand-list"); //商品列表部分
+			this.signOutBtn = $(".sign-out"); //退出按钮
+			this.page = 1; //当前页数
+			this.pageBtn = $(".page a").not(".prev").not(".next"); //页数列表
+			this.prevBtn = $(".prev"); //上一页
+			this.nextBtn = $(".next"); //下一页
+		}
 
-	/**
-	 * 初始化函数
-	 */
-	init() {
-        this.getToolbar();
-        this.getHeader();
-		this.getFooter();
-        this.renderGoodList();
-        this.signOut();
-	}
+		/**
+		 * 初始化函数
+		 */
+		init() {
+			this.getToolbar();
+			this.getHeader();
+			this.getFooter();
+			this.renderGoodList(1);
+			this.changePage();
+            this.prevPage();
+            this.nextPage();
+		}
 
-	/**
-	 * 获取公共部分toolbar
-	 */
-	getToolbar() {
-		this.toolbar.load("toolbar.html");
-    }
-    
-    /**
-	 * 获取公共部分header
-	 */
-	getHeader() {
-		this.header.load("header.html");
-	}
+		/**
+		 * 获取公共部分toolbar
+		 */
+		getToolbar() {
+			this.toolbar.load("toolbar.html");
+		}
 
+		/**
+		 * 获取公共部分header
+		 */
+		getHeader() {
+			this.header.load("header.html");
+		}
 
-	/**
-	 * 获取公共部分footer
-	 */
-	getFooter() {
-		this.footer.load("footer.html");
-	}
+		/**
+		 * 获取公共部分footer
+		 */
+		getFooter() {
+			this.footer.load("footer.html");
+		}
 
-	/**
-	 * 渲染商品列表
-	 */
-	renderGoodList() {
-		$.ajax({
-			url: "http://10.31.162.56/project-juanpi/php/listdata.php",
-			dataType: "json",
-		}).done((data) => {
-			let str = "";
-			for (var i = 0; i < data.length; i++) {
-				str += `
-                    <div class="goods">
-                        <div class="goods-pic">
-                            <a href="detail.html">
-                                <img src="${data[i].url}" alt="${data[i].title}"/>
-                            </a>
+		/**
+		 * 渲染商品列表
+		 */
+		renderGoodList(page) {
+			$.ajax({
+				url: "http://10.31.162.56/project-juanpi/php/listdata.php",
+				data: {
+					page: page,
+				},
+				dataType: "json",
+			}).done((data) => {
+				let str = "";
+				for (var i = 0; i < data.length; i++) {
+					str += `
+                        <div class="goods">
+                            <div class="goods-pic">
+                                <a href="detail.html">
+                                    <img src="${data[i].url}" alt="${data[i].title}"/>
+                                </a>
+                            </div>
+                            <div class="good-price">
+                                <span class="price-current"><em>￥</em>${data[i].newprice}</span>
+                                <span class="des-other">
+                                    <span class="price-old"><em>￥</em>${data[i].oldprice}</span></span
+                                >
+                            </div>
+                            <div class="good-title">
+                                <span class="title-info">${data[i].title}</span>
+                                <span class="title-time">剩3天</span>
+                            </div>
                         </div>
-                        <div class="good-price">
-                            <span class="price-current"><em>￥</em>${data[i].newprice}</span>
-                            <span class="des-other">
-                                <span class="price-old"><em>￥</em>${data[i].oldprice}</span></span
-                            >
-                        </div>
-                        <div class="good-title">
-                            <span class="title-info">${data[i].title}</span>
-                            <span class="title-time">剩3天</span>
-                        </div>
-                    </div>
-                `;
-			}
-			this.brandList.html(str);
-		});
-    }
-    
-    /**
-	 * 退出登录状态
-	 * 清除localStorage
-	 * 页面刷新
-	 */
-	signOut() {
-		this.signOutBtn.on("click", function (e) {
-            console.log(1111)
-			e.preventDefault();
-			localStorage.removeItem("username");
-			window.location.reload();
-		});
+                    `;
+				}
+				this.brandList.html(str);
+			});
+		}
+
+		/**
+		 * 分页功能
+		 */
+		changePage() {
+			let _this = this;
+			$.each(this.pageBtn, function (index, value) {
+				$(value).on("click", function () {
+					_this.page = index + 1;
+                    _this.renderGoodList(_this.page);
+                    console.log(_this.page)
+				});
+			});
+		}
+
+		/**
+		 * 上一页功能
+		 */
+		prevPage() {
+			let _this = this;
+			this.prevBtn.on("click", function () {
+                _this.page--;
+				if ((_this.page < 1)) {
+					_this.page = 1;
+					alert("没有上一页了");
+				}
+				_this.renderGoodList(_this.page);
+			});
+		}
+
+		/**
+		 * 下一页功能
+		 */
+		nextPage() {
+			let _this = this;
+			this.nextBtn.on("click", function () {
+                _this.page++;
+                console.log(_this.page);
+				if ((_this.page > _this.pageBtn.length)) {
+					_this.page = _this.pageBtn.length;
+					alert("没有下一页了");
+				}
+				_this.renderGoodList(_this.page);
+			});
+		}
 	}
-}
+
+	let list = new List();
+	list.init();
+})();
