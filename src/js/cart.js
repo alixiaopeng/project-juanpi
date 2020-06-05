a3d98ac739532c355852ad41b3d05259ed8e5f35
zhujiapeng <297071593@qq.com>
2020-06-05 12:34:22 +0800

    完成了购物车的列表渲染

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,22 +1,19 @@
 (function () {
 	class Cart {
 		constructor() {
-            this.toolbar = $("#toolbar");
-            this.header = $("#header");
-            this.footer = $("#footer");
+			this.header = $("#header");
+			this.footer = $("#footer");
+			this.cart = $(".jgeReN");
+			this.cookieSid = [];
+			this.cookieNum = [];
+			this.goodListArr = [];
 		}
 
 		init() {
-            this.getToolbar();
-            this.getHeader();
-            this.getFooter();
-		}
-
-		/**
-		 * 获取公共部分toolbar
-		 */
-		getToolbar() {
-			this.toolbar.load("toolbar.html");
+			this.getHeader();
+			this.getFooter();
+			this.getCookie();
+            this.getData();
 		}
 
 		/**
@@ -24,13 +21,78 @@
 		 */
 		getHeader() {
 			this.header.load("header.html");
-        }
-        
-        /**
+		}
+
+		/**
 		 * 获取公共部分footer
 		 */
 		getFooter() {
 			this.footer.load("footer.html");
+		}
+
+		/**
+		 * 获取cookie数据
+		 * 将sid和num分别存入对应的数组中
+		 */
+		getCookie() {
+			this.cookieSid = $.cookie("cookieSid").split(",");
+			this.cookieNum = $.cookie("cookieNum").split(",");
+			// console.log(this.cookieSid);
+			// console.log(this.cookieNum);
+		}
+
+		/**
+		 * 通过sid获取对应的数据
+		 */
+		getData() {
+			let _this = this;
+			$.each(this.cookieSid, function (index, value) {
+				$.ajax({
+					url: "http://10.31.162.56/project-juanpi/php/getsid.php",
+					data: {
+						sid: value,
+					},
+					dataType: "json",
+				}).done(function (data) {
+                    let arr = [];
+                    arr.push(data);
+                    $.each(arr,function(index,value){
+                        let str = `
+                            <div class="list-info">
+                                <div class="check">
+                                    <input type="checkbox">
+                                </div>
+                                <div class="pic">
+                                    <a href="#">
+                                        <img width="100" height="100" src="${
+                                            value.url
+                                        }">
+                                    </a>
+                                </div>
+                                <div class="list-style">
+                                    <strong>${value.title}</strong>
+                                </div>
+                                <div class="price">
+                                    ￥<span>${value.newprice}</span>
+                                    /
+                                    ￥<span>${value.oldprice}</span>
+                                </div>
+                                <div class="num">${_this.cookieNum[index]}</div>
+                                <div class="subtotal">
+                                    ￥<span>${
+                                        value.newprice *
+                                        _this.cookieNum[index]
+                                    }</span>
+                                </div>
+                                <div class="operate">
+                                    <span class="delete"></span>
+                                </div>
+                            </div>
+                        `;
+                        _this.cart.append(str)
+                    })
+                });
+            });
 		}
 	}
 
