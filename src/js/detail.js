b24584f4856de8e27e8df973cfb2f350baef9855
zhujiapeng <297071593@qq.com>
2020-06-05 10:03:57 +0800

    完成了详情页存入cookie

--- src/js/detail.js
+++ src/js/detail.js
@@ -17,25 +17,28 @@ class Detail {
 		this.reduceBtn = $(".reduce"); //-按钮
 		this.addBtn = $(".add"); //+按钮
 		this.inp = $(".inp"); //数量输入框
-        this.step = 1;
-        this.sid = location.search.substring(1).split('=')[1];//获取图片sid
-        this.dealWrapTitle = $('.deal-wrap h1');//商品标题
+		this.step = 1;
+		this.sid = location.search.substring(1).split("=")[1]; //获取图片sid
+		this.dealWrapTitle = $(".deal-wrap h1"); //商品标题
+		this.addCartBtn = $(".add-good"); //加入购物车按钮
+		this.arrSid = []; //存储商品的编号sid
+		this.arrNum = []; //存储商品的数量
 	}
 
 	/**
 	 * 初始化函数
 	 */
 	init() {
-        this.getToolbar();
-        this.getHeader();
+		this.getToolbar();
+		this.getHeader();
 		this.getFooter();
 		this.spicHover();
 		this.changeImg();
 		this.rightBtnClick();
 		this.leftBtnClick();
-        this.changNum();
+		this.changNum();
         this.getImg();
-        console.log(this.sid)
+        this.addToCart();
 	}
 
 	/**
@@ -43,9 +46,9 @@ class Detail {
 	 */
 	getToolbar() {
 		this.toolbar.load("toolbar.html");
-    }
-    
-    /**
+	}
+
+	/**
 	 * 获取公共部分header
 	 */
 	getHeader() {
@@ -57,27 +60,28 @@ class Detail {
 	 */
 	getFooter() {
 		this.footer.load("footer.html");
-    }
-    
-    /**
+	}
+
+	/**
 	 * 通过sid获取图片
 	 */
-    getImg(){
-        let _this = this;
-        $.ajax({
-            url: 'http://10.31.162.56/project-juanpi/php/getsid.php',
-            data: {
-                sid: this.sid
-            },
-            dataType: 'json'
-        }).done(function(data) {
-            _this.spicImg.attr('src',data.url);
-            _this.spicImg.attr('alt',data.title);
-            _this.bfImg.attr('src',data.url);
-            _this.bfImg.attr('alt',data.title);
-            _this.dealWrapTitle.text(data.title);
-        });
-    }
+	getImg() {
+		if (!this.sid) this.sid = 1;
+		let _this = this;
+		$.ajax({
+			url: "http://10.31.162.56/project-juanpi/php/getsid.php",
+			data: {
+				sid: this.sid,
+			},
+			dataType: "json",
+		}).done(function (data) {
+			_this.spicImg.attr("src", data.url);
+			_this.spicImg.attr("alt", data.title);
+			_this.bfImg.attr("src", data.url);
+			_this.bfImg.attr("alt", data.title);
+			_this.dealWrapTitle.text(data.title);
+		});
+	}
 
 	/**
 	 * 小图的hover事件
@@ -188,7 +192,7 @@ class Detail {
 
 	/**
 	 * 点击-按钮
-     * 商品数量减1
+	 * 商品数量减1
 	 */
 	reduceBtnClick() {
 		this.reduceBtn.on("click", () => {
@@ -201,7 +205,7 @@ class Detail {
 
 	/**
 	 * 点击+按钮
-     * 商品数量加1
+	 * 商品数量加1
 	 */
 	addBtnClick() {
 		this.addBtn.on("click", () => {
@@ -210,5 +214,41 @@ class Detail {
 			if (value > 99) value = 99;
 			this.inp.val(value);
 		});
+	}
+
+	/**
+	 * 判断是第几次点击(cookie是否存在)
+	 */
+	confirmCookie() {
+		if ($.cookie("cookieSid") && $.cookie("cookieNum")) {
+			this.arrSid = $.cookie("cookieSid").split(","); //获取cookie 同时转换成数组。[1,2,3,4]
+			this.arrNum = $.cookie("cookieNum").split(","); //获取cookie 同时转换成数组。[12,13,14,15]
+		} else {
+			this.arrSid = [];
+			this.arrNum = [];
+		}
+	}
+
+	/**
+	 * 加入购物车
+	 */
+	addToCart() {
+        let _this = this;
+		this.addCartBtn.on("click", function () {
+            _this.confirmCookie();
+            if ($.inArray(_this.sid, _this.arrSid) != -1) { //sid存在，商品列表存在，数量累加
+                //先取出cookie中存在的数量+当前添加的数量，一起添加到cookie中
+                let num = parseInt(_this.arrNum[$.inArray(_this.sid, _this.arrSid)]) + _this.inp.val();
+                _this.arrNum[$.inArray(_this.sid, _this.arrSid)] = num;
+                $.cookie('cookieNum', _this.arrNum, { expires: 10, path: '/' });
+            } else {
+                //第一次点击加入购物车按钮,将商品的sid和商品的数量放到提前准备的数组里面，然后将数组传入cookie.
+                _this.arrSid.push(_this.sid); //将sid放到arrSid数组中
+                $.cookie('cookieSid', _this.arrSid, { expires: 10, path: '/' });
+                _this.arrNum.push(_this.inp.val()); //将数量放到到arrNum数组中
+                $.cookie('cookieNum', _this.arrNum, { expires: 10, path: '/' });
+            }
+            alert('按钮触发了');
+        });
 	}
 }
